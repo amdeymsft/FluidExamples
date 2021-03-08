fb658069edf68890eba51cd2412c4d6cbcaf0642
Amit Dey <amdey@microsoft.com>
2021-03-08 00:18:04 -0800

    dice roller using sharedcell

--- SharedCell/src/dataObject.ts
+++ SharedCell/src/dataObject.ts
@@ -3,14 +3,15 @@
  * Licensed under the MIT License.
  */
 
-import { EventEmitter } from "events";
 import { DataObject, DataObjectFactory } from "@fluidframework/aqueduct";
 import { IValueChanged } from "@fluidframework/map";
+import { SharedCell } from "@fluidframework/cell";
+import { IFluidHandle } from "@fluidframework/core-interfaces";
 
 /**
  * IDiceRoller describes the public API surface for our dice roller data object.
  */
-export interface IDiceRoller extends EventEmitter {
+export interface IDiceRoller {
     /**
      * Get the dice value as a number.
      */
@@ -28,18 +29,21 @@ export interface IDiceRoller extends EventEmitter {
 }
 
 // The root is map-like, so we'll use this key for storing the value.
-const diceValueKey = "diceValue";
 
 /**
  * The DiceRoller is our data object that implements the IDiceRoller interface.
  */
 export class DiceRoller extends DataObject implements IDiceRoller {
+
+    private sharedCell?: SharedCell;
     /**
      * initializingFirstTime is run only once by the first client to create the DataObject.  Here we use it to
      * initialize the state of the DataObject.
      */
     protected async initializingFirstTime() {
-        this.root.set(diceValueKey, 1);
+        const cell = SharedCell.create(this.runtime);
+        cell.set(1);
+        this.root.set("cell", cell.handle);
     }
 
     /**
@@ -47,21 +51,20 @@ export class DiceRoller extends DataObject implements IDiceRoller {
      * DataObject, by registering an event listener for dice rolls.
      */
     protected async hasInitialized() {
-        this.root.on("valueChanged", (changed: IValueChanged) => {
-            if (changed.key === diceValueKey) {
-                // When we see the dice value change, we'll emit the diceRolled event we specified in our interface.
-                this.emit("diceRolled");
-            }
-        });
+        this.sharedCell = await this.root.get<IFluidHandle<SharedCell>>("cell")?.get();
+
+        this.sharedCell?.on("valueChanged", (changed: IValueChanged) => {
+            this.emit("diceRolled");
+          });
     }
 
     public get value() {
-        return this.root.get(diceValueKey);
+        return this.sharedCell?.get();
     }
 
     public readonly roll = () => {
         const rollValue = Math.floor(Math.random() * 6) + 1;
-        this.root.set(diceValueKey, rollValue);
+        this.sharedCell?.set(rollValue)
     };
 }
 
@@ -72,6 +75,6 @@ export class DiceRoller extends DataObject implements IDiceRoller {
 export const DiceRollerInstantiationFactory = new DataObjectFactory(
     "dice-roller",
     DiceRoller,
-    [],
+    [SharedCell?.getFactory()],
     {},
 );
